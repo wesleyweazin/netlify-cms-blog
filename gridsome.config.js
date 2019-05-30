af395d0381c2f5fafeb41b1514901675510ec762
rei0831 <rei.jkoss@gmail.com>
2019-05-30 18:39:10 +0800

    first commmit

---
+++ gridsome.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteName: 'Gridsome',
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        // ...global plugins
+      ]
+    }
+  },
+
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'posts/**/*.md',
+        typeName: 'Post',
+        remark: {
+          plugins: [
+            // ...local plugins
+          ]
+        }
+      }
+    },
+    {
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`
+      }
+    },
+  ]
+}
