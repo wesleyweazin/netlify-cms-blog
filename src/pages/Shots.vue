79fe4d03b4b4ae1e77559d30293e621b550d740d
rei0831 <rei.jkoss@gmail.com>
2020-03-10 23:09:22 +0800

    remove masonary

--- src/pages/Shots.vue
+++ src/pages/Shots.vue
@@ -1,7 +1,6 @@
 <template lang="pug">
 DefaultLayout
     h1 shots
-    v-masonry(transition-duration="0.3s" item-selector=".item" :origin-top="true" :horizontal-order="false")
       .row.no-gutters
         .col-sm-4(v-for='shot in $page.allShot.edges')
           g-image(:src='shot.node.cover').w-100
@@ -10,12 +9,9 @@ DefaultLayout
 </template>
 
 <script>
-import {VueMasonryPlugin} from 'vue-masonry';
 
 export default {
-  components: {
-      VueMasonryPlugin,
-  },
+
 }
 </script>
 <page-query>
